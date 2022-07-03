c444f5b29daf4d9c9fdfcd7aa52e6ce4bfd74cfe
minghui.shen <13030365596@163.com>
2022-07-03 11:05:54 +0800

    context

--- context/main.go
+++ context/main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"golang.org/x/sync/errgroup"
+	"time"
+)
 
+func main() {
+	g := errgroup.Group{}
+	//ctx, cancel := context.WithCancel(context.Background()) // 主动取消
+	ctx, _ := context.WithTimeout(context.Background(), time.Second*5) // 定时取消
+	g.Go(func() error {
+		watchDog(ctx, "watchDog")
+		return nil
+	})
+	time.Sleep(time.Second * 15)
+	//cancel()
+	if err := g.Wait(); err != nil {
+		return
+	}
+}
+
+func watchDog(ctx context.Context, name string) {
+	for {
+		select {
+		case <-ctx.Done():
+			fmt.Println("监控暂停")
+			return
+		default:
+			fmt.Println(name, "监控中")
+		}
+		time.Sleep(time.Second * 1)
+	}
 }
--- go.mod
+++ go.mod
@@ -2,8 +2,4 @@ module learn2
 
 go 1.18
 
-require (
-	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
-	golang.org/x/text v0.3.7 // indirect
-)
+require golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
