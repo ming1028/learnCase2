c00aec9d7942a2e339916a35fbe5caabad50bf7f
minghui.shen <13030365596@163.com>
2022-07-06 17:16:14 +0800

    reflect json

---
+++ jsonStruct/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"strings"
+)
+
+func main() {
+	p := person{
+		Name: "张三",
+		Age:  22,
+	}
+	jsonP, _ := json.Marshal(p)
+	respJson := "{\"name:\":\"李四\",\"age\":50}"
+	json.Unmarshal([]byte(respJson), &p)
+	fmt.Println(string(jsonP), p) // 调用对应的String包
+
+	pt := reflect.TypeOf(p)
+	for i := 0; i < pt.NumField(); i++ {
+		sf := pt.Field(i)
+		fmt.Printf("字段%s, json tag为：%s\n", sf.Name, sf.Tag.Get("json"))
+	}
+
+	pv := reflect.ValueOf(p)
+	pt = reflect.TypeOf(p)
+	jsonBuilder := strings.Builder{}
+	jsonBuilder.WriteString("{")
+	num := pt.NumField()
+	for i := 0; i < num; i++ {
+		jsonTag := pt.Field(i).Tag.Get("json")
+		jsonBuilder.WriteString("\"" + jsonTag + "\"")
+		jsonBuilder.WriteString(":")
+		jsonBuilder.WriteString(fmt.Sprintf("\"%v\"", pv.Field(i)))
+		if i < num-1 {
+			jsonBuilder.WriteString(",")
+		}
+	}
+	jsonBuilder.WriteString("}")
+	fmt.Println(jsonBuilder.String())
+
+	mPrint := pv.MethodByName("Print")
+	fmt.Println(mPrint)
+	args := []reflect.Value{reflect.ValueOf("登陆")}
+	mPrint.Call(args)
+}
+
+func (p person) String() string {
+	return fmt.Sprintf("Name is %s, Age is %d", p.Name, p.Age)
+}
+
+type person struct {
+	Name string `json:"name"`
+	Age  int32  `json:"age"`
+}
+
+func (p person) Print(prefix string) {
+	fmt.Printf("%s:Name is %s,Age is %d\n", prefix, p.Name, p.Age)
+}
---
+++ reflect/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"reflect"
+)
+
+func main() {
+	i := 3
+	iv := reflect.ValueOf(i)
+	it := reflect.TypeOf(i)
+	fmt.Println(iv.CanSet())
+	fmt.Println(iv, it)
+
+	i1 := iv.Interface().(int)
+	fmt.Println(i1)
+
+	ipv := reflect.ValueOf(&i)
+	fmt.Println(ipv.Elem().CanSet())
+	ipv.Elem().SetInt(4)
+	fmt.Println(i)
+
+	p := person{
+		Name: "张三",
+		Age:  20,
+	}
+	ppv := reflect.ValueOf(&p)
+	ppv.Elem().Field(1).SetInt(33)
+	fmt.Println(p)
+
+	fmt.Println(ppv.Kind())
+	pv := reflect.ValueOf(p)
+	fmt.Println(pv.Kind())
+
+	pt := reflect.TypeOf(p)
+	for i := 0; i < pt.NumField(); i++ {
+		fmt.Println("字段：", pt.Field(i).Name)
+	}
+
+	for i := 0; i < pt.NumMethod(); i++ {
+		fmt.Println("方法：", pt.Method(i).Name)
+	}
+
+	stringerType := reflect.TypeOf((*fmt.Stringer)(nil)).Elem()
+	writerType := reflect.TypeOf((*io.Writer)(nil)).Elem()
+	fmt.Println("是否实现fmt.Stringer:", pt.Implements(stringerType))
+	fmt.Println("是否实现io.Writer:", pt.Implements(writerType))
+	fmt.Printf("stringerType:%v\nwriterType:%v\n", stringerType, writerType)
+}
+
+type person struct {
+	Name string
+	Age  int32
+}
+
+// 反射修改一个值的规则
+// 可被寻址，reflect.ValueOf函数传递一个指针作为参数
+// struct结构体字段值需要是可导出的，
+// 使用Elem方法获得指针指向的值，才可以调用Set系列方法进行修改
+
+func (p person) String() string {
+	return fmt.Sprintf("Name is %s, Age is %d", p.Name, p.Age)
+}
