e7cd31a0c71af6469206000c4e6e6ec3a19ba040
minghui.shen <13030365596@163.com>
2022-07-08 10:03:45 +0800

    sliceHeader unsafe.Pointer

--- README.md
+++ README.md
@@ -1,12 +1,19 @@
 ## 个人笔记
+
 ***
+
 ### goroutine
+
 * 只有非休眠（non-sleepine）的Go协程才会被认为是可被调度的；
 * Go协程是以合作式（抢占式）调度来运作的；
 * 所有的Go协程都是匿名的；
+
 ***
+
 ### unsafe
+
 #### unsafe.Pointer
+
 * 任何类型的*T都可以转换为unsafe.Pointer unsafe.Pointer()
 * unsafe.Pointer也可以转换为任何类型的*T (*T)(unsafe.Pointer)
 * unsafe.Pointer可以转换为uintptr unintptr(unsafe.Pointer)
@@ -16,9 +23,42 @@ unsafe.Pointer主要用于指针类型的转换，而且是各个指针类型转
 uintptr主要用于指针运算，尤其是通过偏移量定位不同的内存。
 
 #### unsafe.Sizeof
-返回一个类型所占用的内存大小，这个大小只与类型有关，和类型对应的变量存储的内容大小无关
 
+返回一个类型所占用的内存大小，这个大小只与类型有关，和类型对应的变量存储的内容大小无关
+***
+
+### array
+
+    长度、元素类型组成
+
+* 只有数组内部元素类型和大小一致，才是同一类型
+* 数组作为参数传递是值传递
 
 ***
 
+#### slice
 
+数组和切片都是连续的内存操作，通过索引可以快速找到元素存储的位置
+
+```
+type SliceHeader struct {
+    Data uintptr // 指向存储切片元素的数组
+    Len int // 切片的长度
+    Cap int // 切片的容量
+}
+```
+
+***
+
+#### string和[]byte
+
+Go语言通过先分配一个内存在复制内容的方式，实现string和[]byte之间的强制转换
+
+```
+type StringHeader struct {
+    Data uintptr // 存放指向真实内容的指针
+    Len int
+}
+```
+*SliceHeader可以提供 *StringHeader所需的Data和Len字段，可以转换
+*StringHeader不能转*SliceHeader 缺少Cap字段，需要自己补上
--- slice/main.go
+++ slice/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"fmt"
+	"github.com/spf13/cast"
+	"reflect"
 	"unicode/utf8"
+	"unsafe"
 )
 
 func main() {
@@ -24,4 +27,39 @@ func main() {
 		fmt.Println(idx, st, string(s)) // s unicode字符对应的码点、
 		// for range自动隐式解码unicode字符串
 	}
+
+	a1 := [2]string{"张三", "李四"}
+	s1 := a1[0:1]
+	s2 := a1[:]
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&s1)).Data)
+	fmt.Println((*reflect.SliceHeader)(unsafe.Pointer(&s2)).Data)
+	sli1 := make([]string, 0, 4)
+	// 待定
+	expanSlice(sli1)
+	fmt.Println(sli1)
+
+	s := "张三"
+	fmt.Printf("s的内存地址：%d\n", (*reflect.StringHeader)(unsafe.Pointer(&s)).Data)
+	b := []byte(s)
+	fmt.Printf("b的内存地址：%d\n", (*reflect.StringHeader)(unsafe.Pointer(&b)).Data)
+	s3 := string(b)
+	fmt.Printf("b的内存地址：%d\n", (*reflect.StringHeader)(unsafe.Pointer(&s3)).Data)
+
+	// 大字符串内存开销大，转换思路：不重新申请内存的情况下实现类型转换
+	s = "赵钱孙李"
+	b = []byte(s)
+	s4 := *(*string)(unsafe.Pointer(&b))
+	fmt.Println(s4)
+
+	sh := (*reflect.SliceHeader)(unsafe.Pointer(&s))
+	sh.Cap = sh.Len
+	b1 := *(*[]byte)(unsafe.Pointer(sh)) // b1 和b内容一样，b1没有申请内存，跟s使用同一内存
+	fmt.Println(string(b1))              // b1不可修改，共用内存，string不可修改
+}
+
+func expanSlice(sli []string) {
+	for i := 0; i < 1; i++ {
+		sli = append(sli, "王五"+cast.ToString(i))
+	}
+	fmt.Println(sli)
 }
