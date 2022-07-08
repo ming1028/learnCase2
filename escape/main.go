7b7ec9c8e37dac6e54eec847de18e59c5cc41ad0
minghui.shen <13030365596@163.com>
2022-07-08 15:32:43 +0800

    逃逸

---
+++ escape/main.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+// 逃逸分析
+func main() {
+	fmt.Println("往往") // 被已经逃逸的指针引用的变量也会发生逃逸
+	m := map[int]*string{}
+	s := "飞雪无情"
+	m[0] = &s
+}
+
+// 指针作为函数返回值的时候，一定会发生逃逸
+func newString() *string {
+	s := new(string)
+	*s = "赵钱孙李"
+	return s
+}
+
+func newString2() string {
+	s := new(string)
+	*s = "赵钱孙李"
+	return *s
+}
