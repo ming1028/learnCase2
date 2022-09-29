38abe0847ef862815ee5e3e98176c04ec6eead69
minghui.shen <13030365596@163.com>
2022-09-29 15:55:14 +0800

    interface类型

--- hello/main.go
+++ hello/main.go
@@ -1,8 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
+	var k = 9
+	ll := (*[3]int)(nil)
+	fmt.Println(reflect.TypeOf(ll), reflect.ValueOf(ll))
+	for k = range ll {
+		fmt.Println(k)
+	}
+	fmt.Println(k)
 	fmt.Println("hello world")
 	// 变量声明
 	var vari int = 10
