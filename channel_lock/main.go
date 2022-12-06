2c210bbfc2df4ba13906796b7c40d8c70976274f
minghui.shen <13030365596@163.com>
2022-12-06 13:46:00 +0800

    channel lock timer

--- channel_lock/main.go
+++ channel_lock/main.go
@@ -10,21 +10,22 @@ type Mutex struct {
 	ch chan struct{}
 }
 
+// 初始化
 func NewMutex() *Mutex {
 	mu := &Mutex{
 		make(chan struct{}, 1),
 	}
-	mu.ch <- struct{}{} // 获取不到一直阻塞
+	mu.ch <- struct{}{}
 	return mu
 }
 
 func (m *Mutex) Lock() {
-	<-m.ch // 能输出说明抢到锁
+	<-m.ch // 能输出说明抢到锁，依靠有值输出控制锁的获取,负责会一直阻塞
 }
 
 func (m *Mutex) Unlock() {
 	select {
-	case m.ch <- struct{}{}:
+	case m.ch <- struct{}{}: // 解锁，让Lock可以有值输出
 	default:
 		panic("unlock of unlocked mutex")
 	}
@@ -40,7 +41,7 @@ func (m *Mutex) TryLock() bool {
 }
 
 func (m *Mutex) LockTimeout(timeout time.Duration) bool {
-	timer := time.NewTimer(timeout)
+	timer := time.NewTimer(timeout) // 定时器
 	select {
 	case <-m.ch:
 		timer.Stop()
